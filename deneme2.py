7c78041df2394c60c3529e433a34538904f94b54
Pitirli <168733678+Pitirli@users.noreply.github.com>
2024-05-02 15:27:09 +0300

    Add files via upload

---
+++ deneme2.py
@@ -0,0 +1,60 @@
+import cv2
+
+
+cm = cv2.VideoCapture(0)
+
+
+show_red_area = True
+
+
+red_area = (0, 0, 640, 640)
+
+
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+startTime = None
+endTime = None
+
+while True:
+    ret, frame = cm.read()
+    if not ret:
+        break
+
+    if show_red_area:
+
+        x, y, c, v = red_area
+        cv2.rectangle(frame, (x, y), (x + c, y + v), (0, 0, 255), 2)
+
+
+    gray = cv2.cvtColor(frame[y:y + v, x:x + c], cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    global elapsed_time
+    if len(faces) > 0:
+        for (x1, y1, x2, y2) in faces:
+            cv2.rectangle(frame, (x + x1, y + y1), (x + x1 + x2, y + y1 + y2), (0, 255, 0), 2)
+
+        if startTime is None:
+            startTime = cv2.getTickCount()
+        endTime = None
+        show_red_area = False
+    else:
+        if endTime is None and startTime is not None:
+            endTime = cv2.getTickCount()
+            elapsed_time = (endTime - startTime) / cv2.getTickFrequency()
+            print("Yüz algılanma süresi: {:.2f} saniye".format(elapsed_time))
+
+            startTime = None
+            endTime = None
+            show_red_area = True
+
+        cv2.putText(frame, f'Time: {elapsed_time:.2f}s', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 4)
+
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+
+
+
+cm.release()
+cv2.destroyAllWindows()
